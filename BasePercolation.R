478bda62d6f78b0cf46896be0bc4b1a17a4ba846
lukavom <58477832+lukavom@users.noreply.github.com>
2020-02-12 16:32:36 +0100

    Can now differentiate left-right and top-bottom percolation

--- BasePercolation.R
+++ BasePercolation.R
@@ -1,6 +1,6 @@
 library('plot.matrix')
-set.seed(3)
-L <- 10 #Lattice side length
+set.seed(2)
+L <- 4 #Lattice side length
 p <- 0.5
 
 #initialise and plot lattice
@@ -105,20 +105,22 @@ hasNeighbour <- function(i,j){
 #   }
 # }
 
-
-move <-function(i,j, neighb){
+x<-0
+move <-function(i,j, neighb, left, top){
+  lattice[i,j] <- 2 # mark this node as visited
   neiNew <- neighb
   print("-") 
-  print(sum(neighb))
-  print(sum(neighb[i,j,]))
   print(i)
   print(j)
+  print(sum(neighb))
+  print(sum(neighb[i,j,]))
+  
 
-  if(i == L){
-    print("percolates")
+  if(i == L && top==TRUE){
+    print("percolates") # this will also print "percolates" id the connection is from top to right, or left to bottom etc.
     return(1)
   }
-  if(j == L){
+  if(j == L && left == TRUE){
     print("percolates")
     return(1)
   }
@@ -130,42 +132,42 @@ move <-function(i,j, neighb){
     print("one neigbour")
     if(neighb[i,j,1]==1){
       neiNew[i-1,j,3] <- 0 # remove dead end
-      move(i-1,j, neiNew)
+      move(i-1,j, neiNew, left, top)
     }
     if(neighb[i,j,2]==1){
       neiNew[i,j+1,4] <- 0 # remove dead end
-      move(i,j+1, neiNew)
+      move(i,j+1, neiNew, left, top)
     }
     if(neighb[i,j,3]==1){
       print("hier?")
       neiNew[i+1,j,1] <- 0 # remove dead end
-      move(i+1,j, neiNew)
+      move(i+1,j, neiNew, left, top)
     }
     if(neighb[i,j,4]==1){
       neiNew[i,j-1,2] <- 0 # remove dead end
-      move(i,j-1, neiNew)
+      move(i,j-1, neiNew, left, top)
     }
   }
   
   else if(sum(neighb[i,j,])>1){  #if more than one neighbour it can move anywhere
     print("multiple neigbour")
-    if(neighb[i,j,1]==1){
-      print("top")
-      move(i-1,j, neighb)
-    }
-    else if(neighb[i,j,2]==1){
+    
+    if(neighb[i,j,2]==1){
       print("right")
-      move(i,j+1, neighb)
-    }
-    else if(neighb[i,j,4]==1){ #order changed, caught in top-bottom spiral --> check! nor left-right spiral!
-      print("left")
-      move(i,j-1, neighb)
+      move(i,j+1, neighb, left, top)
     }
     else if(neighb[i,j,3]==1){
       print("bottom")
-      move(i+1,j, neighb)
+      move(i+1,j, neighb, left, top)
     }
-
+    else if(neighb[i,j,4]==1){ #order changed, caught in top-bottom spiral --> check! have checked this, don't know how to prevent either from looping
+      print("left")
+      move(i,j-1, neighb, left, top)
+    }
+    else if(neighb[i,j,1]==1){
+      print("top")
+      move(i-1,j, neighb, left, top)
+    }
   }
 }
 
@@ -182,6 +184,24 @@ for(i in c(1:L)){
 }
 
 #test with a certain start position
-n <- 5
+n <- 2
 m <- 1
-move(n,m, neighbours) # column i, row j
+left <- FALSE
+top <- FALSE
+if( n == 1){
+  assign("top", TRUE, envir=.GlobalEnv)
+} else if(n!=1){
+  assign("top", FALSE, envir=.GlobalEnv)
+}
+if( m == 1){
+  assign("left", TRUE, envir=.GlobalEnv)
+} else if(m!=1){
+  assign("left", FALSE, envir=.GlobalEnv)
+}
+if(lattice[n,m]==1){
+  move(n,m, neighbours, left, top) # column i, row j left, top are bool when start is at left,top
+} else {print("This node is not infected")}
+
+# need to stop the code from going back if no dangling end 
+#need to remeber last step
+# 
