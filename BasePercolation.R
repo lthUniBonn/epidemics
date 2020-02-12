7dc8be1084128fb65f3d65fdd2021d99aa243b33
lthUniBonn <lthimm@posteo.de>
2020-02-12 12:49:08 +0100

    globale neighbours eersetzt, jetzt arbetiet die funktion immer mit neuen neughbours, SPIRALENPROBLEM bei summe>1 , was ist befehl <<-??

--- BasePercolation.R
+++ BasePercolation.R
@@ -3,11 +3,13 @@ set.seed(3)
 L <- 10 #Lattice side length
 p <- 0.5
 
-#initialize lattice
+#initialise and plot lattice
 lattice <- array(rbinom(n = L*L,1,p), dim = c(L,L))
 plot(lattice)
+
+# what are <<-???
 belongsToThisCluster <-array(0,dim = c(L,L))
-neighbours <<- array(0, dim=c(L,L,4))
+neighbours <- array(0, dim=c(L,L,4))
 # isItSpanning()
 
 # isItSpanning <- function(){
@@ -23,11 +25,6 @@ neighbours <<- array(0, dim=c(L,L,4))
 #       }
 #   }
 # }
-isAtLeftEdge <- FALSE
-isAtTopEdge <- FALSE
-isAtBottomEdge <- FALSE
-isAtRightEdge <- FALSE
-
 
 
 hasNeighbour <- function(i,j){
@@ -107,15 +104,16 @@ hasNeighbour <- function(i,j){
 #     return(0)
 #   }
 # }
-n0<-0
-for(i in c(1:L)){
-  for(j in c(1:L)){
-    hasNeighbour(i,j)
-    n0 <- n0+1
-  }
-}
 
-move <-function(i,j){
+
+move <-function(i,j, neighb){
+  neiNew <- neighb
+  print("-") 
+  print(sum(neighb))
+  print(sum(neighb[i,j,]))
+  print(i)
+  print(j)
+
   if(i == L){
     print("percolates")
     return(1)
@@ -124,48 +122,66 @@ move <-function(i,j){
     print("percolates")
     return(1)
   }
-  if(sum(neighbours[i,j,])== 0){
+  if(sum(neighb[i,j,])== 0){
     print("dead end")
     return(0)
   }
-  else if(sum(neighbours[i,j,])==1){  #if only one neighbour its a dangling end and should be removed before moving on
-  
-    if(neighbours[i,j,1]==1){
-      neighbours[i-1,j,3] <<- 0 # remove dead end
-      move(i-1,j)
+  else if(sum(neighb[i,j,])==1){  #if only one neighbour its a dangling end and should be removed before moving on
+    print("one neigbour")
+    if(neighb[i,j,1]==1){
+      neiNew[i-1,j,3] <- 0 # remove dead end
+      move(i-1,j, neiNew)
     }
-    if(neighbours[i,j,2]==1){
-      neighbours[i,j+1,4] <<- 0 # remove dead end
-      move(i,j+1)
+    if(neighb[i,j,2]==1){
+      neiNew[i,j+1,4] <- 0 # remove dead end
+      move(i,j+1, neiNew)
     }
-    if(neighbours[i,j,3]==1){
-      neighbours[i+1,j,1] <<- 0 # remove dead end
-      move(i+1,j)
+    if(neighb[i,j,3]==1){
+      print("hier?")
+      neiNew[i+1,j,1] <- 0 # remove dead end
+      move(i+1,j, neiNew)
     }
-    if(neighbours[i,j,4]==1){
-      neighbours[i,j-1,2] <<- 0 # remove dead end
-      move(i,j-1)
+    if(neighb[i,j,4]==1){
+      neiNew[i,j-1,2] <- 0 # remove dead end
+      move(i,j-1, neiNew)
     }
   }
   
-  else if(sum(neighbours[i,j,])<1){  #if more than one neighbour it can move anywhere
-    
-    if(neighbours[i,j,1]==1){
-      move(i-1,j)
+  else if(sum(neighb[i,j,])>1){  #if more than one neighbour it can move anywhere
+    print("multiple neigbour")
+    if(neighb[i,j,1]==1){
+      print("top")
+      move(i-1,j, neighb)
     }
-    if(neighbours[i,j,2]==1){
-      move(i,j+1)
+    else if(neighb[i,j,2]==1){
+      print("right")
+      move(i,j+1, neighb)
     }
-    if(neighbours[i,j,3]==1){
-      move(i+1,j)
+    else if(neighb[i,j,4]==1){ #order changed, caught in top-bottom spiral --> check! nor left-right spiral!
+      print("left")
+      move(i,j-1, neighb)
     }
-    if(neighbours[i,j,4]==1){
-      move(i,j-1)
+    else if(neighb[i,j,3]==1){
+      print("bottom")
+      move(i+1,j, neighb)
     }
+
+  }
+}
+
+isAtLeftEdge <- FALSE
+isAtTopEdge <- FALSE
+isAtBottomEdge <- FALSE
+isAtRightEdge <- FALSE
+n0<-0
+for(i in c(1:L)){
+  for(j in c(1:L)){
+    hasNeighbour(i,j)
+    n0 <- n0+1
   }
 }
 
 #test with a certain start position
-n <- 1
+n <- 5
 m <- 1
-move(n,m)
+move(n,m, neighbours) # column i, row j
