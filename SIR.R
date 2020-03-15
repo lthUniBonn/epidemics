23b1f56a234010593ec13235fb715092e2a689fc
lukavom <58477832+lukavom@users.noreply.github.com>
2020-03-15 18:14:26 +0100

    added age structure

--- SIR.R
+++ SIR.R
@@ -4,34 +4,42 @@ source('modules.R')
 
 #startTime <- proc.time()
 #---------- Parameters to be set ----------------
-profile <- profvis({
-set.seed(3)
+#profile <- profvis({
 
-immunity <- 0.3 #ratio of immune people 
-bondOccProb <- 0.99
-avgRecoveryTime <- 3.5
+#set.seed(1)
 
+immunity <- 0 #ratio of immune people 
+bondOccProb <- 0.8
+
+
+#!! match recovery with age?? 
+avgRecoveryTime <- 3.55
+sdRecoveryTime <- 1
+
+sAgeDist <- c(1, 0.7, 0.5, 0.7, 0.8, 1)/3.5 #susceptibility depending on age
+#!! bond prob dep on age dist
 #network
 N = 400**2 # number of people
 nShort <- 0 # how many shortcuts are created // not impossibly many, could lead to ~inf loop #!!
-openBoundaries <- TRUE # if FALSE the opposing edges of the lattice are connected (periodic)
+periodicBoundaries <- TRUE #if FALSE the opposing edges of the lattice are connected (periodic)
 
 sBool <- FALSE # if True the susceptibility is 1 or 0 // other poss like gaussian with age etc
 sFixed <- FALSE
-sNot <- TRUE
-
+sNot <- FALSE
+sReal <- TRUE
 #observables
-#plotting
+   #plotting
 plotIt <- FALSE
 plotEvery <- 100
 plotAccumulated <- TRUE
-#clusters
+   #clusters
 checkCluster <- TRUE
-clusterEvery <- 100
+clusterEvery <- 10
 
 count <- 0
 auswertungsVector <- array(0, dim=c(1000,8), dimnames = list(c(),c("time","largestCluster", "numberCluster","numberInfected","largeOverTotal","largeOverRest","accInfections", "R0")))
-
+R0OverInfectiousPeriod <- numeric(length=N)
+R0Mean <- numeric(10000)
 # pFrom <- 0 # which canonical Q(p) are created
 # pTo <- 1
 # nProb <- 40 # how many datapoints are calculated in the above range
@@ -39,15 +47,16 @@ auswertungsVector <- array(0, dim=c(1000,8), dimnames = list(c(),c("time","large
 
 
 #-----------initialise network---------------------
-lattice <- array(data=c(1:N), dim = c(sqrt(N),sqrt(N),3),dimnames = list(c(),c() , c("id", "I", "S")))
+lattice <- array(data=c(1:N), dim = c(sqrt(N),sqrt(N)))
 
-if(!openBoundaries){# these are the possible connections in 2D lattice
+if(periodicBoundaries){# these are the possible connections in 2D lattice
   possConn <- array(0,dim=c((2*N+nShort),3)) 
 } else {
   possConn <- array(0,dim=c((2*N-2*sqrt(N)+nShort),3))
 }
 #set Infection status
 infected <- logical(length = N)
+infectionTime <- numeric(length=N)
 
 #set susceptibility distribution in population
 if(sBool){
@@ -58,124 +67,49 @@ if(sFixed){
 }
 if(sNot){
   sDistribution <- sample(c(0.25,0.5,0.75,1), replace=TRUE, size=N,prob = c((1-immunity)/8,  (1-immunity)/2, (1-immunity)/4, (1-immunity)/8))
+#!! implement a scaling for the susc  // age
 }
+if(sReal){
+  ageDistribution <- sample(c(3, 20,40,60,80, 100), replace=TRUE, size=N,prob = c(3, 15, 25, 28, 22, 7))
+  sDistribution <- unlist(lapply(ageDistribution,FUN = sAge))
+}
+
+
+
+
+
+
+#save Distribution of initially susceptible people for eval
 initialsDistribution <- sDistribution
-lattice[,,3] <- sDistribution # set a uniform suceptibility 
-#at the beginning nobody is infected
-lattice[,,2] <- 0
-infectionTime <- numeric(length=N)
 
-#find all possible connections and their likelihoods
+#find all possible connections and their bond probability
 findConn()
-
 nCon <- nrow(possConn)
-
-#initialise observables
-#binoms <- array(0, dim=c(nCon,nProb))
-#percolTest <- array(0, dim = c(nCon,nTest))
-#largestWeight <- array(0, dim = c(nCon,nTest))
-#initializeBinoms()
 
 # infect Patient 0
 patientZero()
 
-timesteps <- function(){
-  #infected people
-  infPeople <- which(infected==TRUE)
-  if (length(infPeople) == 0){
-    stop(... = "No more infected", call. = TRUE)
-  }
-
-  
-  #infected connections
-  #infConn <- possConn[which(possConn[,1] %in% infPeople | possConn[,2] %in% infPeople),c(1,2,3)]
-                                              #will hier ^  ein xor
-  # schmeisse alle raus wo beide sites infiziert sind
-  
-  #infConn <- possConn[which(xor(possConn[,1] %in% infPeople, possConn[,2] %in% infPeople)),c(1,2,3)]
-  
-  a <- possConn[,1] %in% infPeople
-  b <- possConn[,2] %in% infPeople
-
-  infConn <- possConn[which(xor(a, b)),c(1,2,3)]
-  
-  infBondProb <- infConn[,3]
-  possiblyInf1 <- infConn[,1]
-  possiblyInf2 <- infConn[,2]
-  
-  susc1 <- sDistribution[possiblyInf1]
-  susc2 <- sDistribution[possiblyInf2]
-  
-  # test for bond probability
-  possiblyInf1 <- possiblyInf1[which(infBondProb >= runif(n = nrow(infConn)))]
-  possiblyInf2 <- possiblyInf2[which(infBondProb >= runif(n = nrow(infConn)))]
-  
-  #randVec <- runif(n = nrow(infConn))
-  #randVec2 <- runif(n = nrow(infConn))
-  # test for susceptibility
-  newlyInf1 <- possiblyInf1[which(susc1 >= 3.000000*runif(n = nrow(infConn)))]
-  newlyInf2 <- possiblyInf2[which(susc2 >= 3.000000*runif(n = nrow(infConn)))]
-  
-  
-  #recovery // remove people
-  recoveryCheck <- findRecBool(infectionTime[infected])
-  recPeople <- infPeople[recoveryCheck]
-  
-  # check if people who are recovering are newly infected and dismiss these infections
-  
-  # an effective %in% is setdiff:
-  newlyInf1 <- setdiff(newlyInf1,recPeople)
-  newlyInf2 <- setdiff(newlyInf2,recPeople)
-  
-  infectionTime[recPeople] <<- 0
-  sDistribution[recPeople] <<- 0
-
-  #increase infection time 
-  primaryInfected <- length(which(infected == TRUE))
-  infected[recPeople] <<- FALSE
-  infected[newlyInf1] <<- TRUE
-  infected[newlyInf2] <<- TRUE
-  newlyInfected <- length(which(infected == TRUE))
-  R0 <- (newlyInfected-primaryInfected)/primaryInfected
-  
-  infectionTime[infected] <<- infectionTime[infected] + 1
-  return(R0)
-}
-
-
-
-findRecBool <- function(t){
-  #p <- 0.5-1/t**2
-  return(t>=rnorm(length(t), mean = avgRecoveryTime, sd =1)) #!! more realistic: sample gaussian? 
-
-  #if(t >= 2){return(TRUE)} else {return(FALSE)}
-}
 
 x <- 0
 while (TRUE) {
-
-  
   R0 <- timesteps()
-  if((x %% plotEvery == 0) && (plotIt == TRUE)){
-    visibleLattice <- array(0, dim= c(sqrt(N),sqrt(N)))
-    visibleLattice[which(infectionTime != 0)] <- 1
-    plot(which(visibleLattice==1, arr.ind = TRUE)[,1], which(visibleLattice==1, arr.ind = TRUE)[,2],xlim = c(0,sqrt(N)), ylim = c(0,sqrt(N)))
-    #plot(visibleLattice)
-  }
+  # returns NA if no more infected
+  if(is.na(R0)){ break}
+  # accumulated plot of infected | currently infected in red
   if((x %% plotEvery == 0) && (plotAccumulated == TRUE)){
     visibleLattice <- array(0, dim= c(sqrt(N),sqrt(N)))
     visibleLattice[which(sDistribution  != initialsDistribution)] <- 1
     plot(which(visibleLattice==1, arr.ind = TRUE)[,1], which(visibleLattice==1, arr.ind = TRUE)[,2],xlim = c(0,sqrt(N)), ylim = c(0,sqrt(N)),type ="p", pch = '.', pty = "s")
-    # par(new=TRUE)
-    # visibleLattice[which(infected == TRUE)] <- 1
-    # plot(which(visibleLattice==1, arr.ind = TRUE)[,1], which(visibleLattice==1, arr.ind = TRUE)[,2],xlim = c(0,sqrt(N)), ylim = c(0,sqrt(N)),type ="p", pch = '.',col = 'red', pty = "s")
-    # #plot(visibleLattice)
+    par(new=TRUE)
+    visibleLattice[which(infected == TRUE)] <- 2
+    plot(which(visibleLattice==2, arr.ind = TRUE)[,1], which(visibleLattice==2, arr.ind = TRUE)[,2],xlim = c(0,sqrt(N)), ylim = c(0,sqrt(N)),type ="p", pch = '*',col = 'red', pty = "s")
   }
+  # evaluation of disease parameters
   if ((x %% clusterEvery == 0) && (checkCluster == TRUE)){
     count <- count +1
     people <- c(1:N)
-    weight <- numeric(N) #rep(1,N)
-    # now identifiy clusters as before, but instead of the connections sampled from possConn use infConn
+    weight <- numeric(N) 
+    # now identifiy clusters use Newman Algo with infConn as used bonds
     infPeople <- which(infectionTime != 0)
     weight[infPeople] <- 1
     infConn <- possConn[which((possConn[,1] %in% infPeople) & (possConn[,2] %in% infPeople)),c(1,2)]
@@ -194,22 +128,12 @@ while (TRUE) {
     auswertungsVector[count,6] <- weight[which.max(weight)]/sum(weight[-which.max(weight)]) #! is this right?
     auswertungsVector[count,7] <- (length(which(sDistribution != initialsDistribution))+length(infPeople))/N
     auswertungsVector[count,8] <- R0
-    # print(paste("largest cluster:" , weight[which.max(weight)], sep = " "))
-    # print(paste("Number cluster:" , length(which(weight!=0)), sep = " "))
-    # print(paste("Infected:" , sum(weight), sep = " "))
-    # print(paste("Largest cluster over total infected:" , weight[which.max(weight)]/sum(weight), sep = " "))
-    # print(paste("Largest cluster over smaller clusters:" , weight[which.max(weight)]/sum(weight[-which.max(weight)]), sep = " "))
-    #!! interessantes in auswertungsvektor schreiben? 
-     
   }
   
   x <- x + 1
 }
 
-# tau <- avgRecoveryTime
-# x<- ?
-# 
-# R0 <- x*S*tau/N
+auswertungsVector <- auswertungsVector[c(1:count),]
 plot(auswertungsVector[,1],auswertungsVector[,2], xlab = "T",ylab = "largest Cluster")
 plot(auswertungsVector[,1],auswertungsVector[,3], xlab = "T",ylab = "Number of Clusters")
 plot(auswertungsVector[,1],auswertungsVector[,4], xlab = "T",ylab = "Number Infected")
@@ -217,42 +141,19 @@ plot(auswertungsVector[,1],auswertungsVector[,5], xlab = "T",ylab = "Largest Clu
 plot(auswertungsVector[,1],auswertungsVector[,6], xlab = "T",ylab = "Largest Cluster over Smaller Clusters")
 plot(auswertungsVector[,1],auswertungsVector[,7], xlab = "T",ylab = "Attack Rate")
 plot(auswertungsVector[,1],auswertungsVector[,8], xlab = "T",ylab = "R0")
-#plot(auswertungsVector[,1],plot(auswertungsVector[,8])
-#plot(auswertungsVector[,1],plot(auswertungsVector[,9])
-# snapshot <- function(){
-#   identify clusters
-#   measure observables
-# }
-
-#!! cluster identification (rework)
-# for (a in c(1:nTest)){
-#   people <- c(1:N)
-#   weight <- rep(1,N)
-#   maxWeight <- 1
-#   connections <- possConn[sample(nCon,nCon,replace=FALSE,prob = possConn[,3]),c(1,2)] #choose bonds which will be occupied gradually
-#   for(x in c(1:nCon)){ 
-#     if(checkLargestCluster){ # this if is really just for comfort, can remove it if it takes too long
-#       newWeight <- addConnection(connections[x,2], connections[x,1])
-#       if(newWeight > maxWeight){
-#         maxWeight <- newWeight
-#       }
-#       largestWeight[x,a] <- maxWeight
-#       #largestWeight[x,a] <- weight[which.max(weight)] # this is sligthly faster than max(weight)
-#     }else {
-#       addConnection(connections[x,2], connections[x,1])
-#     }
-#   }
-#   
-# }
 
 #endTime <- proc.time()
 #runTime <- endTime-startTime
 #write(x = c(N, runTime[1]), file = "timesNewman.txt", append = TRUE,sep = "\t")
 
 
-})
+#})
 
 #-----------------------------------------------------------------------------------
 #observed that for large (>100) lattices the boundaries are basically irrelevant for the largest cluster size
-# bond occupation probablitiy include? in infection / in possCOnn.. ?? at all?? ->  transmissibility? 
-# transmissiblity: the same as S?
+
+
+# auswertungsvektor in file 
+
+
+
--- modules.R
+++ modules.R
@@ -5,10 +5,89 @@ library('Brobdingnag')
 
 
 patientZero <- function(){
-  zeroId <- sample(x = which(sDistribution == 1),size = 1) #choose one of the susceptibles to be patient 0
+  zeroId <- sample(x = which(sDistribution != 0),size = 1) #choose one of the susceptibles to be patient 0
   infected[zeroId] <<- TRUE
   #latticeID <- which(lattice[,,1] == zeroId, arr.ind = TRUE)
   #lattice[latticeID[1], latticeID[2],2] <<- 1 
+}
+
+findRecBool <- function(t){
+  return(t>=rnorm(length(t), mean = avgRecoveryTime, sd = sdRecoveryTime))
+}
+
+timesteps <- function(){
+  # find infected people
+  infPeople <- which(infected==TRUE)
+  if (length(infPeople) == 0){
+    print("no more infected")
+    return(NA)
+  }
+  
+  
+  # find infected connections
+  # only get bonds where exactly one site is infected
+  a <- possConn[,1] %in% infPeople
+  b <- possConn[,2] %in% infPeople
+  infConn <- possConn[which(xor(a, b)),c(1,2,3)]
+  
+  # split infConn for convenience
+  infBondProb <- infConn[,3]
+  possiblyInf1 <- infConn[,1]
+  possiblyInf2 <- infConn[,2]
+  
+  # test for bond probability
+  bondCheck <- runif(n = nrow(infConn))
+  possiblyInf1 <- possiblyInf1[which(infBondProb >= bondCheck)]
+  possiblyInf2 <- possiblyInf2[which(infBondProb >= bondCheck)]
+  
+  susc1 <- sDistribution[possiblyInf1]
+  susc2 <- sDistribution[possiblyInf2]
+  
+  # test for susceptibility
+  newlyInf1 <- possiblyInf1[which(susc1 >= runif(n = length(possiblyInf1)))] 
+  newlyInf2 <- possiblyInf2[which(susc2 >= runif(n = length(possiblyInf2)))]
+  
+  #recovery // remove people
+  recoveryCheck <- findRecBool(infectionTime[infected])
+  recPeople <- infPeople[recoveryCheck]
+  infectionTime[recPeople] <<- 0
+  
+  # recovered people are now immune
+  sDistribution[recPeople] <<- 0
+  
+  
+  # check if people who are recovering are newly infected and dismiss these infections
+  #newlyInf1 <- setdiff(newlyInf1,recPeople)
+  #newlyInf2 <- setdiff(newlyInf2,recPeople)
+  
+  
+  
+  primaryInfected <- length(infPeople)
+  
+  infected[newlyInf1] <<- TRUE
+  infected[newlyInf2] <<- TRUE
+  totalInfected <- length(which(infected == TRUE))
+  
+  infected[recPeople] <<- FALSE
+  #increase infection time 
+  infectionTime[infected] <<- infectionTime[infected] + 1
+  
+  R0 <- (totalInfected-primaryInfected)/primaryInfected
+  #statistical participation in disease spread for this timestep
+  
+  R0OverInfectiousPeriod[infPeople] <<- R0OverInfectiousPeriod[infPeople] + R0
+  R0Mean[x] <<- mean(R0OverInfectiousPeriod[which(R0OverInfectiousPeriod!=0)])#!! check!! 
+  
+  return(R0)
+}
+
+sAge <- function(age){
+  if (age <= 3){return(sAgeDist[1])}
+  else if (age <= 20){return(sAgeDist[2])}
+  else if (age <= 40){return(sAgeDist[3])}
+  else if (age <= 60){return(sAgeDist[4])}
+  else if (age <= 80){return(sAgeDist[5])}
+  else if (age <= 100){return(sAgeDist[5])}
 }
 
 # find possible connections in 2d case next neighbor
@@ -19,69 +98,64 @@ findConn <- function(){
     for(i in c(1:(sqrt(N)-1))){
       
       counter <- counter + 1
-      possConn[counter,1] <<- lattice[i,j,1]  
-      possConn[counter,2] <<- lattice [i+1,j,1]
-      possConn[counter,3] <<- transProb(lattice[i,j,3],lattice [i+1,j,3])
+      possConn[counter,1] <<- lattice[i,j]  
+      possConn[counter,2] <<- lattice [i+1,j]
       counter <- counter +1 
-      possConn[counter,1] <<- lattice[i,j,1] 
-      possConn[counter,2] <<- lattice [i,j+1,1]
-      possConn[counter,3] <<- transProb(lattice[i,j,3],lattice [i,j+1,3])
+      possConn[counter,1] <<- lattice[i,j] 
+      possConn[counter,2] <<- lattice [i,j+1]
     }
     counter <- counter + 1
-    possConn[counter,1] <<- lattice[sqrt(N),j,1]  
-    possConn[counter,2] <<- lattice [sqrt(N),j+1,1] 
-    possConn[counter,3] <<- transProb(lattice[sqrt(N),j,3],lattice [sqrt(N),j+1,3])
+    possConn[counter,1] <<- lattice[sqrt(N),j]  
+    possConn[counter,2] <<- lattice [sqrt(N),j+1] 
+    
   }
   for (i in c(1:(sqrt(N)-1))){
     counter <- counter + 1
-    possConn[counter,1] <<- lattice[i,sqrt(N),1]  
-    possConn[counter,2] <<- lattice [i+1,sqrt(N),1]
-    possConn[counter,3] <<- transProb(lattice[i,sqrt(N),3],lattice[i+1,sqrt(N),3])
+    possConn[counter,1] <<- lattice[i,sqrt(N)]  
+    possConn[counter,2] <<- lattice [i+1,sqrt(N)]
   }
   
-  if(!openBoundaries){
+  if(periodicBoundaries){
     for(i in c(1:sqrt(N))){
       counter <- counter + 1
-      possConn[counter,1] <<- lattice[1,i,1]
-      possConn[counter,2] <<- lattice[sqrt(N),i,1]
-      possConn[counter,3] <<- transProb(lattice[1,i,3],lattice[sqrt(N),i,3])
+      possConn[counter,1] <<- lattice[1,i]
+      possConn[counter,2] <<- lattice[sqrt(N),i]
     }
     for(i in c(1:sqrt(N))){
       counter <- counter + 1
-      possConn[counter,1] <<- lattice[i,1,1]
-      possConn[counter,2] <<- lattice[i,sqrt(N),1]
-      possConn[counter,3] <<- transProb(lattice[i,1,3],lattice[i,sqrt(N),3])
+      possConn[counter,1] <<- lattice[i,1]
+      possConn[counter,2] <<- lattice[i,sqrt(N)]
     }
   }
   if(nShort != 0){   
     while (TRUE) {
-        if(nShort != 0){
-          counter <- counter + 1
-          condition <- TRUE
-          while (condition == TRUE) {
-            fromRow <- sample(c(1:sqrt(N)), size = nShort, replace = TRUE)
-            fromCol <- sample(c(1:sqrt(N)), size = nShort, replace = TRUE)
-            toRow   <- sample(c(1:sqrt(N)), size = nShort, replace = TRUE)
-            toCol   <- sample(c(1:sqrt(N)), size = nShort, replace = TRUE)
-            for (i in c(1:nShort)){
-              if (!(fromRow[i] == toRow[i] && fromCol[i] == toCol[i])){
-                condition <- FALSE
-                break
-              }
+      if(nShort != 0){
+        counter <- counter + 1
+        condition <- TRUE
+        while (condition == TRUE) {
+          fromRow <- sample(c(1:sqrt(N)), size = nShort, replace = TRUE)
+          fromCol <- sample(c(1:sqrt(N)), size = nShort, replace = TRUE)
+          toRow   <- sample(c(1:sqrt(N)), size = nShort, replace = TRUE)
+          toCol   <- sample(c(1:sqrt(N)), size = nShort, replace = TRUE)
+          for (i in c(1:nShort)){
+            if (!(fromRow[i] == toRow[i] && fromCol[i] == toCol[i])){
+              condition <- FALSE
+              break
             }
           }
-          for (i in c(0:(nShort-1))){
-          possConn[counter + i,1] <<- lattice[fromRow[i+1],fromCol[i+1],1]
-          possConn[counter + i,2] <<- lattice[toRow[i+1],toCol[i+1],1]
-          possConn[counter + i,3] <<- transProb(lattice[fromRow[i+1],fromCol[i+1],3],lattice[toRow[i+1],toCol[i+1],3])
-        # not left < right with shortcuts #!!
-          }
+        }
+        for (i in c(0:(nShort-1))){
+          possConn[counter + i,1] <<- lattice[fromRow[i+1],fromCol[i+1]]
+          possConn[counter + i,2] <<- lattice[toRow[i+1],toCol[i+1]]
+          
+          # not left < right with shortcuts #!!
+        }
         counter <- counter - 1
         if (!(TRUE %in% duplicated(possConn))){break}
-        }
+      }
     }
   }
-  # possConn <<- possConn[-which(possConn[,3]==0),] only works if which retruns something! only needed if 0 possible
+  possConn[,3] <<- mapply(FUN = transProb, possConn[,1], possConn[,2])
 }
 
 #!! change later // get bond occupation probability  
@@ -90,7 +164,7 @@ transProb <- function(x,y){ #this function determines the likelihood of transmis
 }
 
 
-    #cluster evaluation
+#cluster evaluation
 #------
 findRoot <- function(startIndex){
   #find root plus path compression
@@ -133,50 +207,4 @@ addConnection <- function(from, to){ # does this work with shortcuts? I think ye
   return(weight[toRoot])
 }
 
-#------
-
-erf <- function(x,a,b) (pnorm(a*(x-b) * sqrt(2)))
-
-# High efficient Newman from microcanonical to canonical (binomial interpret) 
-canonical <- function(micObs){#find p from n
-  canObs <- numeric(nProb)
-  i <- 0
-  for (p in c(1:nProb)){ 
-    i <- i+1
-    canObs[i] <- sum(binoms[,i]*micObs)
-  }
-  return(canObs)
-}
-
-initializeBinoms <- function(){
-  i <- 0
-  print(nCon)
-  for (p in seq(pFrom, pTo, (pTo-pFrom)/(nProb-1))){
-    i <- i+1
-    binoms[,i] <<- dbinom(x = c(1:nCon),size = nCon, prob = p) 
-  }
-}
-
-
-#----------- display data -----------------
-
-plotLargestCLuster <- function(){
-  xData <- seq(pFrom, pTo, (pTo-pFrom)/(nProb-1))
-  yData <- array(0,c(nProb,nTest))
-  ySEM <- numeric(nProb)
-  for(i in c(1:nTest)){
-    yData[,i] <- canonical(largestWeight[,i]) 
-  }
-  for(i in c(1:nProb)){
-    ySEM[i] <- sd(yData[i,])/(sqrt(nTest)*N)
-  }
-  yMean <- rowMeans(yData)/N
-  yData <- yData/N
-  largestCluster <- data.frame( x= xData, y=yMean)
-  plot(largestCluster,xlab = "", ylab = "",xlim = c(0,1), ylim = c(0,1))
-  arrows(xData, yMean-ySEM, xData, yMean+ySEM, length=0.05, angle=90, code=3) 
-  title(main="Largest Cluster Size",  xlab="Q(p)", ylab="Size") 
-  text(x = pFrom, y = 1, labels = paste("N:", N,"  nShort:", nShort, "  nTest:", nTest, "do not trust errorbars yet",sep = " "),pos = 4)
-  abline(h=1-immunity)
-}
 
